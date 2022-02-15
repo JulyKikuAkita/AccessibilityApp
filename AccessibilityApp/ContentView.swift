2f38f0bd6e2b1f4a787329c65cc544497e63c6d2
JulyKikuAkita <b92701105@gmail.com>
2022-02-14 23:20:06 -0800

    Hiding and grouping accessibility data

--- AccessibilityApp/ContentView.swift
+++ AccessibilityApp/ContentView.swift
@@ -4,7 +4,16 @@
 //
 //  Created by Ifang Lee on 2/14/22.
 //
+/**
+ How active VoiceOver user use the app:
+ 1. they are remarkably adept at navigating around user interfaces,
+ 2. they also often set reading speed extremely fast
 
+ ways to adopt:
+ Marking images as being unimportant for VoiceOver.
+ Hiding views from the accessibility system.
+ Grouping several views as one.
+ */
 import SwiftUI
 
 struct ContentView: View {
@@ -25,15 +34,32 @@ struct ContentView: View {
     @State private var selectedPicture = Int.random(in: 0...3)
 
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .onTapGesture {
-                selectedPicture = Int.random(in: 0...3)
+        VStack {
+            Image(decorative: "myFutureNoodles") //ignored by VoiceOver
+                .accessibilityHidden(true) //make the view complete invisible to VoiceOver
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
             }
-            .accessibilityLabel(labels[selectedPicture])
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
+            //.accessibilityElement(children: .combine) //read two lines together with little pause
+            .accessibilityElement(children: .ignore) // alternatives to use combine children
+            .accessibilityLabel("Your score is 1000") //no pause
+            //.ignore is the default parameter for children: .accessibilityElement(children: .ignore) == .accessibilityElement().
+
+
+
+            Image(pictures[selectedPicture])
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    selectedPicture = Int.random(in: 0...3)
+                }
+                .accessibilityLabel(labels[selectedPicture])
+                .accessibilityAddTraits(.isButton)
+                .accessibilityRemoveTraits(.isImage)
+        }
+
     }
 }
 
