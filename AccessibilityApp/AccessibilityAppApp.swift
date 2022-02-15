be38da36fe3a21205eb20def45d3091127af5be6
JulyKikuAkita <b92701105@gmail.com>
2022-02-14 17:01:14 -0800

    Initial Commit

---
+++ AccessibilityApp/AccessibilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityAppApp.swift
+//  AccessibilityApp
+//
+//  Created by Ifang Lee on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
